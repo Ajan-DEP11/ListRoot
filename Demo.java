e229df9d981b031653e67e262bb78b87ae15611a
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-15 11:17:31 +0530

    second

--- Demo.java
+++ Demo.java
@@ -1,21 +1,39 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 
 public class Demo {
-    public static void main(String[] args) {
+    private static Path path;
+
+    public static void main(String[] args)throws IOException {
 
         //io 
        File[] listRoots = File.listRoots();
        System.out.println(Arrays.toString(listRoots));
 
+       for(File file : listRoots){
+        File[] rootContent = file.listFiles();
+        for(File f : rootContent){
+            System.out.println(f);
+        }
+       }
+
+       System.out.println("==============================");
+
        //nio
 
        Iterable<Path> rootDirectories =FileSystems.getDefault().getRootDirectories();
        for(Path path : rootDirectories){
+
         System.out.println(path);
+
+        DirectoryStream<Path> folderContent = Files.newDirectoryStream(path);
+        folderContent.forEach(p -> System.out.println(p.toAbsolutePath()));
        }
 
     }
